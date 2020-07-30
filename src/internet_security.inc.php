b9da052d5bcafa9dad7c1e83f59d27deb228e190
joserobjr <joserobjr@gamemods.com.br>
2020-07-30 14:04:28 -0300

    Anonymize sockets

--- src/internet_security.inc.php
+++ src/internet_security.inc.php
@@ -1,7 +1,7 @@
 <?php
 function anonymize_ips($content) {
     $ips = array();
-    $content = preg_replace_callback('@(\[/)([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})(:)@', function ($matches) use ($ips) {
+    $anonymize_ip = function ($matches) use ($ips) {
         $ip = $matches[2];
         if (isset($ips[$ip])) {
             return $matches[1].$ips[$ip].$matches[3];
@@ -9,6 +9,18 @@ function anonymize_ips($content) {
         $anonymous = "1.1.1.".sizeof($ips);
         $ips[$ip] = $anonymous;
         return $matches[1].$anonymous.$matches[3];
+    };
+
+    $content = preg_replace_callback('@(\[/)?([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})(:)@', $anonymize_ip, $content);
+
+    $sockets = array();
+    $content = preg_replace_callback('@([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}:)([0-9]+)@', function ($matches) use ($sockets) {
+        if (isset($sockets[$matches[0]])) {
+            return $sockets[$matches[0]];
+        }
+        $anonymous = $matches[1].(sizeof($sockets)+1000);
+        $sockets[$matches[0]] = $anonymous;
+        return $anonymous;
     }, $content);
 
     $content = preg_replace_callback('@(\s*(?:server-(?:ip|port)|level-seed|(?:sub-)?motd)\s*=)[^\n\r]+@', function ($matches) {
