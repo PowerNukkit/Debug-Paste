f6a56c5c3a80f28686034a121398a48f502c6a68
joserobjr <joserobjr@gamemods.com.br>
2020-07-30 20:18:13 -0300

    Adds a small debug

--- src/github/issue_comment_edited.php
+++ src/github/issue_comment_edited.php
@@ -10,11 +10,10 @@ function handle_issue_comment_edited($hook) {
         curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'PATCH');
         curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode(array('body' => $new_body)));
         curl_setopt($curl, CURLOPT_HTTPHEADER, array(
-            'Content-Type: application/json',
-            'Accept: application/vnd.github.machine-man-preview+json',
+            'Accept: application/vnd.github.v3+json',
             'Authorization: Bearer ' . create_jwt()
         ));
-        print_r(curl_exec($curl));
+        file_put_contents(__DIR__.'/../../last_edit.txt', print_r(curl_exec($curl), true));
         curl_close($curl);
     }
 }
