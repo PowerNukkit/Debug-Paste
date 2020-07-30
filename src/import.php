45c1ca1aaa2b71877328829b1fde4ab4d6d5bcc4
joserobjr <joserobjr@gamemods.com.br>
2020-07-30 11:59:00 -0300

    Enable error reporting

--- src/import.php
+++ src/import.php
@@ -4,6 +4,10 @@ if (!isset($_GET['haste'])) {
     die("Missing data");
 }
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 require_once 'import.inc.php';
 $haste = $_GET['haste'];
 if (!preg_match("@https://hastebin\.com/[a-zA-Z0-9]+(\.[a-zA-Z0-9]+)?@", $haste)) {
