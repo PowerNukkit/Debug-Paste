eb86b7fe67347cfd76fc3193b51adf838d398b38
joserobjr <joserobjr@gamemods.com.br>
2020-07-30 12:56:32 -0300

    Fixes ip anonymization again

--- src/internet_security.inc.php
+++ src/internet_security.inc.php
@@ -1,13 +1,13 @@
 <?php
 function anonymize_ips($content) {
     $ips = array();
-    return preg_replace_callback('@(?:\[/|\b)[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}(?:\b|:)@', function ($matches) use ($ips) {
-        $ip = $matches[0];
+    return preg_replace_callback('@(\[/)([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})(:)@', function ($matches) use ($ips) {
+        $ip = $matches[1];
         if (isset($ips[$ip])) {
-            return $ips[$ip];
+            return $matches[0].$ips[$ip].$matches[2];
         }
         $anonymous = "1.1.1.".sizeof($ips);
         $ips[$ip] = $anonymous;
-        return $anonymous;
+        return $matches[0].$anonymous.$matches[2];
     }, $content);
 }
