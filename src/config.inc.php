6ef3fa73e9729ee90965a6c26056701eba4b8ba5
joserobjr <joserobjr@gamemods.com.br>
2020-07-30 18:18:09 -0300

    Make the hook replace hastebin links on edit

--- src/config.inc.php
+++ src/config.inc.php
@@ -3,3 +3,39 @@ define('PASTES_DIR', __DIR__.'/pastes');
 if (!is_dir(PASTES_DIR) && !mkdir(PASTES_DIR)) {
     die("Failed to create the pastes dir!");
 }
+
+function create_jwt() {
+    if (!defined('APP_ID') && !defined('APP_KEY')) {
+        http_response_code(500);
+        die('App config issue');
+    }
+
+    //Header Token
+    $header = [
+        'typ' => 'JWT',
+        'alg' => 'HS256'
+    ];
+
+    //Payload - Content
+    $issued_at_time = (new DateTime("now"))->getTimestamp();
+    $payload = [
+        'iat' => $issued_at_time,
+        'exp' => $issued_at_time + 20,
+        'iss' => APP_ID
+    ];
+
+    //JSON
+    $header = json_encode($header);
+    $payload = json_encode($payload);
+
+    //Base 64
+    $header = base64_encode($header);
+    $payload = base64_encode($payload);
+
+    //Sign
+    openssl_private_encrypt($header . "." . $payload, $sign, APP_KEY);
+    $sign = base64_encode($sign);
+
+    //Token
+    return $header . '.' . $payload . '.' . $sign;
+}
--- src/github/issue_edited.php
+++ src/github/issue_edited.php
@@ -1,4 +1,21 @@
 <?php
-function handle_issue_edited($hook) {
+require_once '../import.inc.php';
+require_once '../config.inc.php';
 
+function handle_issue_edited($hook) {
+    $new_body = auto_import_pastes($hook['issue']['body']);
+    if ($new_body != $hook['issue']['body']) {
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $hook['url']);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'PATCH');
+        curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode(array('body' => $new_body)));
+        curl_setopt($curl, CURLOPT_HTTPHEADER, array(
+            'Content-Type: application/json',
+            'Accept: application/vnd.github.machine-man-preview+json',
+            'Authorization: Bearer '.create_jwt()
+        ));
+        curl_exec($curl);
+        curl_close($curl);
+    }
 }
