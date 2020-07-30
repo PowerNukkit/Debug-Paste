ecb21cdff726041eb93ba76a0139dde0e90169d8
joserobjr <joserobjr@gamemods.com.br>
2020-07-30 15:26:48 -0300

    Fix hook security

--- src/github_hook.php
+++ src/github_hook.php
@@ -1,10 +1,22 @@
 <?php
-require_once '../github_secret.php';
+@require_once '../github_secret.php';
 
-if (!isset($_SERVER['HTTP_SECRET_TOKEN']) || sha1(GITHUB_SECRET) != $_SERVER['HTTP_SECRET_TOKEN']) {
-    http_response_code(403);
-    file_put_contents('../debug.txt', print_r($_SERVER, true));
+if (!defined('GITHUB_SECRET')) {
+    http_response_code(500);
     die();
+}
+
+if (!isset($_SERVER['HTTP_X_HUB_SIGNATURE'])
+    || !isset($_SERVER['HTTP_X_GITHUB_EVENT'])
+    || !isset($_SERVER['HTTP_X_GITHUB_DELIVERY'])
+    || strlen($_SERVER['HTTP_SECRET_TOKEN']) < 6
+    || substr($_SERVER['HTTP_SECRET_TOKEN'], 0, 5) != 'sha1='
+    || sha1(GITHUB_SECRET) != substr($_SERVER['HTTP_SECRET_TOKEN'], 5)
+    || $_SERVER['HTTP_X_GITHUB_EVENT'] != 'issue_comment'
+) {
+    http_response_code(403);
+    file_put_contents('../last_failure.txt', print_r($_SERVER, true));
+    die('Illegal input');
 }
 
 $hook = json_decode(file_get_contents('php://input'), true);
