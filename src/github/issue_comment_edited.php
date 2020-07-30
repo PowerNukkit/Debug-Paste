3d620d7537c0ccbb342c2d167899a30d5a0515ad
joserobjr <joserobjr@gamemods.com.br>
2020-07-30 20:21:26 -0300

    Adds more debug

--- src/github/issue_comment_edited.php
+++ src/github/issue_comment_edited.php
@@ -5,15 +5,18 @@ function handle_issue_comment_edited($hook) {
     $new_body = auto_import_pastes($hook['comment']['body']);
     if ($new_body != $hook['comment']['body']) {
         $curl = curl_init();
-        curl_setopt($curl, CURLOPT_URL, $hook['comment_url']);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'PATCH');
-        curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode(array('body' => $new_body)));
-        curl_setopt($curl, CURLOPT_HTTPHEADER, array(
-            'Accept: application/vnd.github.v3+json',
-            'Authorization: Bearer ' . create_jwt()
-        ));
-        file_put_contents(__DIR__.'/../../last_edit.txt', print_r(curl_exec($curl), true));
+        $opts = array (
+            CURLOPT_URL => $hook['comment_url'],
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_CUSTOMREQUEST => 'PATCH',
+            CURLOPT_POSTFIELDS => json_encode(array('body' => $new_body)),
+            CURLOPT_HTTPHEADER => array(
+                'Accept: application/vnd.github.v3+json',
+                'Authorization: Bearer ' . create_jwt()
+            )
+        );
+        curl_setopt_array($curl, $opts);
+        file_put_contents(__DIR__.'/../../last_edit.txt', print_r($opts, true)."\n\n".print_r(curl_exec($curl), true));
         curl_close($curl);
     }
 }
