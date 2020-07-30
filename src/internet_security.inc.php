f81176412cff9ea739fc37184ddc29e99d1065c6
joserobjr <joserobjr@gamemods.com.br>
2020-07-30 12:58:12 -0300

    Fixes ip anonymization groups

--- src/internet_security.inc.php
+++ src/internet_security.inc.php
@@ -2,12 +2,12 @@
 function anonymize_ips($content) {
     $ips = array();
     return preg_replace_callback('@(\[/)([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})(:)@', function ($matches) use ($ips) {
-        $ip = $matches[1];
+        $ip = $matches[2];
         if (isset($ips[$ip])) {
-            return $matches[0].$ips[$ip].$matches[2];
+            return $matches[1].$ips[$ip].$matches[3];
         }
         $anonymous = "1.1.1.".sizeof($ips);
         $ips[$ip] = $anonymous;
-        return $matches[0].$anonymous.$matches[2];
+        return $matches[1].$anonymous.$matches[3];
     }, $content);
 }
