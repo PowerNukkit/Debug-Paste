7544f5307859804209f7c2df072275e19ee32a9b
joserobjr <joserobjr@gamemods.com.br>
2020-07-30 13:07:01 -0300

    Anonymize sensitive data from server.properties

--- src/internet_security.inc.php
+++ src/internet_security.inc.php
@@ -1,7 +1,7 @@
 <?php
 function anonymize_ips($content) {
     $ips = array();
-    return preg_replace_callback('@(\[/)([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})(:)@', function ($matches) use ($ips) {
+    $content = preg_replace_callback('@(\[/)([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})(:)@', function ($matches) use ($ips) {
         $ip = $matches[2];
         if (isset($ips[$ip])) {
             return $matches[1].$ips[$ip].$matches[3];
@@ -10,4 +10,10 @@ function anonymize_ips($content) {
         $ips[$ip] = $anonymous;
         return $matches[1].$anonymous.$matches[3];
     }, $content);
+
+    $content = preg_replace_callback('@^(\s*(?:server-(?:ip|port)|level-seed|(?:sub-)?motd)\s*=).*@', function ($matches) {
+        return $matches[1].'#auto-removed#';
+    }, $content);
+
+    return $content;
 }
