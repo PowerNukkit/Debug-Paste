aeb520c58cd11c75fb2ff2be521a76b37d6cc174
joserobjr <joserobjr@powernukkit.org>
2020-10-07 10:57:53 -0300

    Adds some debug information

--- src/paste.php
+++ src/paste.php
@@ -1,3 +1,9 @@
 <?php
 header("HTTP/1.1 201 Created");
-echo "https://debugpaste.powernukkit.org/testing";
+echo "https://debugpaste.powernukkit.org/testing\n";
+echo "POST: \n";
+print_r($_POST);
+
+echo "\nFILES:\n";
+print_r($_FILES);
+
