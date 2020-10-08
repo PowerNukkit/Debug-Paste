39da4b8474974f55142ff144fc5be9462d392555
joserobjr <joserobjr@powernukkit.org>
2020-10-08 11:59:08 -0300

    Implements the final logic of the paste.php

--- src/import.inc.php
+++ src/import.inc.php
@@ -28,7 +28,7 @@ function auto_import_pastes($content) {
 
 function make_html($content, $code) {
     return "<html><head>
-<meta http-equiv='Content-Type' content='text/html;charset=UTF-8'><title>Debug Paste $code</title>
+<meta http-equiv='Content-Type' content='text/html;charset=UTF-8'><title>Debug Paste ".htmlspecialchars($code)."</title>
 <link rel=\"stylesheet\"
       href=\"//cdnjs.cloudflare.com/ajax/libs/highlight.js/10.1.2/styles/default.min.css\">
 <script src=\"//cdnjs.cloudflare.com/ajax/libs/highlight.js/10.1.2/highlight.min.js\"></script>
@@ -46,4 +46,19 @@ function restore_powernukkit_links($content) {
 
 function add_to_queue($url) {
     import_haste($url);
+}
+
+function random_token($length = 32){
+    if(!isset($length) || intval($length) <= 8 ){
+        $length = 32;
+    }
+    if (function_exists('random_bytes')) {
+        return binde(random_bytes($length));
+    }
+    if (function_exists('mcrypt_create_iv')) {
+        return bin2hex(mcrypt_create_iv($length, MCRYPT_DEV_URANDOM));
+    }
+    if (function_exists('openssl_random_pseudo_bytes')) {
+        return bin2hex(openssl_random_pseudo_bytes($length));
+    }
 }
--- src/paste.php
+++ src/paste.php
@@ -1,4 +1,6 @@
 <?php
+require_once 'import.inc.php';
+
 if ($_SERVER['REQUEST_METHOD'] != 'PUT') {
     header("HTTP/1.1 405 Method Not Allowed");
     header("Allow: PUT");
@@ -28,7 +30,7 @@ if (!$disk_free_space || $disk_free_space < $len * 40) {
     exit(1);
 }
 
-$input = @fopen("php://input", "r");
+$input = @fopen("php://input", "rb");
 if (!$input) {
     header("HTTP/1.1 400 Bad Request");
     exit;
@@ -47,12 +49,19 @@ if(!unlink($tmp_dir) || !mkdir($tmp_dir)) {
     exit(6);
 }
 
+$extracted = "$tmp_dir/extracted";
+if (!mkdir($extracted)) {
+    header("HTTP/1.1 500 Internal Server Error");
+    error_log("Failed to create dir $extracted");
+    exit(7);
+}
+
 register_shutdown_function(function () {
     global $tmp_dir;
     delTree($tmp_dir);
 });
 
-$output = fopen("$tmp_dir/upload.zip", "w");
+$output = fopen("$tmp_dir/upload.zip", "wb");
 if (!$output) {
     header("HTTP/1.1 500 Internal Server Error");
     error_log("Failed to open the file for writing: $tmp_dir/upload.zip");
@@ -78,11 +87,77 @@ while ($data = fread($input,1024)) {
     }
 }
 
-abort();
+@fclose($output);
+@fclose($input);
+
+$code = "PN".random_token();
+$attempts = 0;
+while (is_dir("pastes/$code")) {
+    if (++$attempts > 1000) {
+        header("HTTP/1.1 500 Internal Server Error");
+        error_log("Failed to create a random code");
+        abort();
+        exit(8);
+    }
+    $code = "PN".random_token();
+}
+
+if(!mkdir("pastes/$code")) {
+    header("HTTP/1.1 500 Internal Server Error");
+    error_log("Failed to the folder pastes/$code");
+    abort();
+    exit(9);
+}
+
+$gz_files = array(
+    'server-second-most-latest.log.gz'
+);
+
+$direct_files = array(
+    'nukkit.yml',
+    'server.properties',
+    'server-info.txt',
+    'server-latest.log',
+    'status.txt',
+    'thread-dump.txt',
+);
+
+if(!file_put_contents("$extracted/.htaccess", "Options +Indexes\n")) {
+    error_log("Failed to create $extracted/.htaccess");
+}
+
+foreach ($gz_files as $gz_file) {
+    $full_path = "$tmp_dir/$gz_file";
+    if (is_file($full_path)) {
+        if (!gz_inflate($full_path)) {
+            error_log("Failed inflate $gz_file.");
+        } else {
+            $direct_files[] = substr($gz_file, 0, -3);
+        }
+    }
+}
+
+foreach ($direct_files as $direct_file) {
+    $full_path = "$tmp_dir/$direct_file";
+    if (is_file($full_path)) {
+        $contents = file_get_contents($full_path);
+        if(!file_put_contents("$extracted/$direct_file.html", make_html($contents, "$code/$direct_file"))) {
+            error_log("Failed to write to $extracted/$direct_file.html");
+        }
+    }
+}
+
+if(!rename($extracted, "pastes/$code")) {
+    header("HTTP/1.1 500 Internal Server Error");
+    error_log("Failed to move $extracted to pastes/$code");
+    abort();
+    exit(10);
+}
+
+delTree($tmp_dir);
 
 header("HTTP/1.1 201 Created");
-echo "https://debugpaste.powernukkit.org/testing\n";
-echo $total;
+echo "https://debugpaste.powernukkit.org/pastes/$code";
 
 function abort() {
     global $input, $output, $tmp_dir;
@@ -92,6 +167,52 @@ function abort() {
     @unlink($output);
     
     delTree($tmp_dir);
+}
+
+/**
+ * @param $from_file string Gz file path
+ * @param string|null $to_file string Path to the output file
+ * @return bool If the operation was successful
+ * @throws Exception If from_file or to_file are invalid
+ */
+function gz_inflate(string $from_file, string $to_file = null) {
+    if (!$to_file) {
+        if (substr_compare(mb_strtolower($from_file), ".gz", -3) === 0) {
+            $to_file = substr($from_file, 0, -3);
+        }
+
+    }
+    if (!$from_file || !$to_file || mb_strtolower($to_file) == mb_strtolower($from_file)) {
+        throw new Exception("Missing input or output file. Input: $from_file, Output: $to_file");
+    }
+    
+    if (!is_file($from_file)) {
+        throw new Exception("The file $from_file does not exists");
+    }
+
+    $buffer_size = 4096;
+    $input = gzopen($from_file, 'rb');
+    $output = fopen($to_file, 'wb');
+    try {
+        if (!$input || !$output) {
+            error_log("Failed to decompress $from_file, could not open input($input) or output($output).");
+            return false;
+        }
+
+        while(!gzeof($input)) {
+            if (!fwrite($output, gzread($input, $buffer_size))) {
+                error_log("Failed to decompress $from_file. fwrite failed.");
+                @fclose($output);
+                unlink($output);
+                return false;
+            }
+        }
+        
+        return true;
+    } finally {
+        @fclose($output);
+        @gzclose($input);
+    }
 }
 
 function delTree($dir) {
